bb46313054744697291fad204a5e2d3e00c65404
NicoDbGt94 <nico_toro_xd@hotmail.com>
2025-05-25 12:45:29 -0300

    Primer commit al buffer, prueba unitaria

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import morgan from 'morgan';
+import {join,dirname} from 'path';
+import { fileURLToPath } from 'url';
+
+
+//initializations
+const app = express();
+const __dirname=dirname(fileURLToPath(import.meta.url));
+//settings
+app.set('port', process.env.PORT || 3000);
+app.set('views',join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//routes
+app.get('/',(req,res)=>{
+  res.render("index")
+})
+
+//static files
+app.use(express.static(join(__dirname, 'public')));
+
+//starting the server
+app.listen(app.get('port'), () => {
+  console.log('Server on port', app.get('port'));
+});
