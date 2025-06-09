682936226600f280a06421d85fcda69ab94621f1
NicoDube94 <nicomas.09@gmail.com>
2025-06-09 01:08:24 -0300

    agregado de la view de editar y agregar persona, estan funcionando correctamente

--- src/routes/personas.routes.js
+++ src/routes/personas.routes.js
@@ -13,5 +13,43 @@ router.get("/list", async (req,res)=>{
 
 })
 
+router.get("/add", (req,res)=>{
+  res.render("personas/add");
+})
 
+router.post("/add", async (req,res)=>{
+  try{
+    const {nombre, apellido, edad} = req.body;
+    const persona = {nombre, apellido, edad};
+    console.log(persona);
+    if(persona){
+      await pool.query("INSERT INTO persons SET ?", [persona]);
+      res.redirect("/list");
+    }
+    
+  }catch(error){
+    res.status(500).json({message: error.message});
+  }
+})
+
+router.get("/edit/:id", async (req,res)=>{
+  try{
+    const id = req.params.id;
+    const persona = await pool.query("SELECT * FROM persons WHERE id = ?", [id]);
+    res.render("personas/edit", {persona: persona[0][0]});
+  }catch(error){
+    res.status(500).json({message: error.message});
+  }
+})
+router.post("/edit", async (req,res)=>{
+  try{
+    const id = req.body.id;
+    const {nombre, apellido, edad} = req.body;
+    const persona = {nombre, apellido, edad};
+    await pool.query("UPDATE persons SET ? WHERE id = ?", [persona, id]);
+    res.redirect("/list");
+  }catch(error){
+    res.status(500).json({message: error.message});
+  }
+})
 export default router;
